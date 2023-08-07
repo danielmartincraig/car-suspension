ecbc8c9f6e1f211b4522fd99cc9e5bd718048aea
danielmartincraig <danielmartincraig@gmail.com>
2023-08-07 13:45:48 -0500

    added emmy dependency

--- src/app/db.cljs
+++ src/app/db.cljs
@@ -1,4 +1,14 @@
-(ns app.db)
+(ns app.db
+  (:require 
+   [emmy.matrix :as matrix :refer [s->m]]))
+
+(def laplacian-matrix
+  (matrix/by-rows [6.3 -3.7 -2.6 0 0]
+                  [-8.9 10.1 0 -1.2 0]
+                  [0 0 4.2 -1.9 -2.3]
+                  [0 0 0 0 0]
+                  [-4.4 0 0 -2.7 7.1]))
 
 (def default-db
-  {:todos (sorted-map-by >)})
+  {:todos (sorted-map-by >)
+   :springs laplacian-matrix})
--- src/app/subs.cljs
+++ src/app/subs.cljs
@@ -2,5 +2,9 @@
   (:require [re-frame.core :as rf]))
 
 (rf/reg-sub :app/todos
-  (fn [db _]
-    (:todos db)))
+            (fn [db _]
+              (:todos db)))
+
+(rf/reg-sub :app/springs
+            (fn [db _]
+              (:springs db)))
